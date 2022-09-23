11e4ddaec5c6f3a00a618e11761544dfc262d87c
ksyzz <837818491@qq.com>
2022-09-23 16:07:09 +0800

    123

---
+++ dataset/__init__.py
@@ -0,0 +1,7 @@
+from dataset.nlp.JsonFromFiles import JsonFromFilesDataset
+from dataset.nlp.MultiJsonFromFiles import MultiJsonFromFilesDataset
+
+dataset_list = {
+    "JsonFromFiles": JsonFromFilesDataset,
+    "MultiJsonFromFiles": MultiJsonFromFilesDataset
+}
