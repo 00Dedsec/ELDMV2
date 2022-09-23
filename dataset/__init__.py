a673714ae66a9a3eca79210b349a85ac2492f336
00DedSec <00DedSec>
2022-09-23 16:42:44 +0800

    init

--- dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-from dataset.nlp.JsonFromFiles import JsonFromFilesDataset
-from dataset.nlp.MultiJsonFromFiles import MultiJsonFromFilesDataset
-
-dataset_list = {
-    "JsonFromFiles": JsonFromFilesDataset,
-    "MultiJsonFromFiles": MultiJsonFromFilesDataset
-}
