d51d9c1a1f269aa7a9f0568770b591d895421ad9
00Dedsec <631038893@qq.com>
2022-10-13 20:08:42 +0800

    PLI init

---
+++ student/model/PLI/PLI.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoModel
+
+class PLIMoudle(nn.Module):
+    def __init__(self, config, gpu_list, *args, **params):
+        super(PLIMoudle, self).__init__()
+        self.encoder = AutoModel.from_pretrained("bert-base-chinese")
+        self.fc = nn.Linear(config.getint("data","max_seq_length"), 4)
+
+    
+    def forward(self, data):
+        q_input_ids = data['q_input_ids']
+        c_input_ids = data['c_input_ids']
+        q_attention_mask = data['q_attention_mask']
+        c_attention_mask = data['c_attention_mask']
+        
+        q = self.encoder(input_ids=q_input_ids, attention_mask=q_attention_mask)
+        c = self.encoder(input_ids=c_input_ids, attention_mask=c_attention_mask)
+        q_last_hidden_state = q.last_hidden_state
+        c_last_hidden_state = c.last_hidden_state
+
+        c_t = torch.transpose(c_last_hidden_state, 1, 2)
+        re = torch.bmm(q_last_hidden_state, c_t) # [batch_size, max_len, max_len]
+        re = torch.softmax(re, dim=2)
+        re = torch.max(re, dim=1)[0] #[batch, hidden_size]
+
+        re = self.fc(re)
+        return re
+
+class PLI(nn.Module):
+    def __init__(self, config, gpu_list, *args, **params):
+        super(PLI, self).__init__()
+        self.PLIMoudle = PLIMoudle(config, gpu_list, *args, **params)
+        self.loss = nn.CrossEntropyLoss()
+
+    def init_multi_gpu(self, device, config, *args, **params):
+        self.PLIMoudle = nn.DataParallel(self.PLIMoudle, device_ids=device)
+    
+    def forward(self, data, config, gpu_list, mode, *args, **params):
+        re = self.PLIMoudle(data)
+        if mode == 'train' or mode == 'valid':
+            label = data['label']
+            # label = label.to(torch.float)
+            loss = self.loss(re, label)
+            return re, loss
+        else:
+            return re
+    
